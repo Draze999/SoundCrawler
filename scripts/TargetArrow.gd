97f276dad69774ebe64f8488671bbee2098139d4
Aurelien Masset <aurelien.masset@epitech.eu>
2023-11-28 15:03:54 +0100

    Added Mouse support for the Target Arrow (need testing for the controller)

--- scripts/TargetArrow.gd
+++ scripts/TargetArrow.gd
@@ -12,8 +12,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	direction.x = Input.get_axis("Aim_Left", "Aim_Right")
-	direction.y = Input.get_axis("Aim_Up", "Aim_Down")
+	
+	var controllers =  Input.get_connected_joypads()
+	if controllers.is_empty():
+		var mouse_pos = get_viewport().get_mouse_position()
+		var player_pos = get_node("../../Player").position
+		var CamPos = get_viewport().get_camera_2d().get_screen_center_position()
+		var positioncam = CamPos - (get_viewport_rect().size / 2)
+		direction.x = - player_pos.x + mouse_pos.x + positioncam.x
+		direction.y = - player_pos.y + mouse_pos.y + 64 + positioncam.y
+		direction = direction.normalized()
+	else:
+		direction.x = Input.get_axis("Aim_Left", "Aim_Right")
+		direction.y = Input.get_axis("Aim_Up", "Aim_Down")
 	distance = sqrt((direction.x * direction.x) + (direction.y * direction.y))
 	clamp(distance, 0, 1)
 	set_scale(Vector2(distance, distance))
