9ef0dc6386ff4468a54d521bd384b46ea4dcf6e0
SolangeLL <solange.le-luel@epitech.eu>
2023-11-30 16:02:25 +0100

    Add basic patrol and collision layers

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-@export var patrol_type: PackedStringArray = ["Loop", "Linear"]
-@onready var path_follow = get_parent()
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var moveSpeed = 200
 
 func _ready():
 	$AnimatedSprite2D.play("idle")
+
+func _process(delta):
+	velocity.x = moveSpeed
+	move_and_slide()
+	
+	if is_on_wall():
+		moveSpeed *= -1
+		$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h 
